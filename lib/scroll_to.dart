2f21621ba6c6f84e2f51f0680a1cef2f20fd5c47
Gerald Nuraj <gerald.nuraj@crispybacon.al>
2025-07-15 16:53:17 +0200

    refactor: Put ScrollToListView  in a separate file

--- lib/scroll_to.dart
+++ lib/scroll_to.dart
@@ -5,4 +5,5 @@ export 'src/scroll_animation_type.dart';
 export 'src/scroll_easing.dart';
 export 'src/scroll_to_config.dart';
 export 'src/scroll_to_controller.dart';
+export 'src/scroll_to_list_view.dart';
 export 'src/scroll_to_widget.dart';
